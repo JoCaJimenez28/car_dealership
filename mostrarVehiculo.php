d939f2dc97a6c6a0227975e502b7f918a96e5975
José Carlos Jiménez <jimenez.jcarlos28@gmail.com>
2023-03-02 22:48:07 -0700

    subiendo mostrarVehiculo

---
+++ mostrarVehiculo.php
@@ -0,0 +1,55 @@
+<head>
+<style>
+table {
+border-collapse: collapse;
+}
+table, td, th {
+border: 1px solid black;
+padding: 5px;
+}
+th {
+text-align: left;
+}
+</style>
+</head>
+<body>
+<?php
+$con = mysqli_connect('localhost','Lozi','1234'); 
+if (!$con) {
+die('No se pudo conectar: ' . mysqli_error($con)); 
+}
+mysqli_select_db($con,'dbt');
+$sql="SELECT * FROM datos"; 
+$result = mysqli_query($con,$sql); 
+echo "<table>
+<tr>
+<th>Placa</th>
+<th>Tipo de vehículo</th>
+<th>Marca</th>   
+<th>Modelo</th>
+<th>Versión del modelo</th>
+<th>Número de Serie</th>
+<th>Accion</th>
+</tr>";
+while($ren = mysqli_fetch_array($result)) { 
+echo "<tr>";
+echo '<td id="idren">' . $ren['id'] . '</td>';
+echo "<td>" . $ren['placa'] . "</td>";
+echo "<td>" . $ren['tipoDeVehiculo'] . "</td>";
+echo "<td>" . $ren['marca'] . "</td>";
+echo "<td>" . $ren['modelo'] . "</td>";
+echo "<td>" . $ren['versionDeModelo'] . "</td>";
+echo "<td>" . $ren['numeroDeSerie'] . "</td>";
+echo "<td>";
+echo " <form method='Post'>";
+ echo " <input type='image' src='img/borrar.jpg' value='Borra Todo' 
+ onclick='borrarVehiculo33(".$ren['placa'].")'>";
+echo " </form>";
+echo "</td>";
+echo "</tr>";
+}
+echo "</table>";
+mysqli_close($con);
+?>
+</body>
+</html>
